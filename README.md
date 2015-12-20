2591b4ce4a713440f3733c4aa973e5d26e28be29
Dylan Michael Wheatley <dylan@esquilax>
2015-12-19 18:58:42 -0800

    Clean up for Github

---
+++ README.md
@@ -0,0 +1,3 @@
+A small fibonacci generator that will produce a fibonacci sequence using any
+	two arbitrary values, as well as an upper bound. The program accepts
+	both positive and negative kernels.
--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,89 +1,60 @@
-#include "../std_lib_facilities.h"
+#include <iostream>
+#include <vector>
 
+/* TODO
+- fix -1, 1/1, -1 bug
+*/
 
-// creates a vector populated with factors of x found in v
-vector<int> factors(const int x, const vector<int> v){
-	vector<int> facts;
-	for (int i = 0; i < v.size(); ++i){
-		if (v[i] != 0 && v[i] != 1 && v[i] != 2 && v[i] != -1 && v[i] != -2 
-				&& v[i] != x && x % v[i] == 0){	//messy garbage to exclude trivial factors
-			facts.push_back(v[i]);
-		}
-	}
-
-	return facts;
-}
-
-// a function that takes 2 ints, a vector to populate, and a limit as its arguments
-void fibonacci(int& x, int& y, vector<int>& v, int n){
+// function to produce a fibonacci sequence using two arbitrary initial values
+void fibonacci(int& x, int& y, std::vector<int>& v, int n){
 	int t = 0;
-	v.push_back(x); // populate v with our intial values
-	v.push_back(y);
 
-	if (n > x + y && x + y > 0)
+	// filter out nonsense inputs (eg: -1, -1, 300)
+	if (n > x + y && x + y >= 0) {
+		v.push_back(x);	// populate v with our initial variables
+		v.push_back(y);
 		while (x + y <= n){
 			t = x + y;
 			x = y;
 			y = t;
 			v.push_back(t);
 		}
-	else if (n < x + y && x + y < 0)
+	}
+	else if (n < x + y && x + y <= 0) {
+		v.push_back(x);
+		v.push_back(y);
 		while (x + y >= n){
 			t = x + y;
 			x = y;
 			y = t;
 			v.push_back(t);
 		}
-	else cout << "Bad input pal\n";
+	}
+	else std::cout << "Bad input\n";
 }
 
 
 int main(){
-	while (true){
-		// declare our variables
-		vector<int> vfibonacci;
-		int a = 0;
-		int b = 0;
-		int n = 0;
+	// declare & initialize our variables
+	std::vector<int> vfibonacci;
+	int a = 0;
+	int b = 1;
+	int n = 0;
 
-		cout << "Gimme two ints and a stop pal" << endl;
-		cin >> a >> b >> n;
-		fibonacci(a, b, vfibonacci, n);
-		//include factors?
-		char choice;
-		cout << "Include factors?";
-		cin >> choice;
-		switch (choice){
-			case 'y':
-			// cycle through all the elements in vfibonacci except the last
-			for (int i = 0; i < (vfibonacci.size() - 1); ++i){
-				cout << vfibonacci[i] << ", ";
-				// build a sub vector of all elements of vfibonacci up to vfibonacci[i]
-				vector<int> subvfib;
-				for (int j = 0; j <= i; ++j)
-					subvfib.push_back(vfibonacci[j]);
-				// build a vector of factors of vfibonacci[i] found in subvfib, and output them
-				vector<int> facts = factors(vfibonacci[i], subvfib);
-				if (facts.size() > 0){
-					cout << "factors of " << vfibonacci[i] << ": (";
-					for (int k = 0; k < facts.size(); ++k)
-						cout << facts[k] << " ";
-					cout << ")";
-				}
-			}
-			break;
-			case 'n':
-			for (int i =0; i < (vfibonacci.size() -1); ++i)
-				cout << vfibonacci[i] << ", ";
-			break;
-			default:
-			cout << "bad input";
-			break;
-		}
-		// print out the last element, with (slightly) different formatting
-		cout << vfibonacci.back() << ".";
-		cout << endl;
+	std::cout << "Please provide two integers to start the sequence, ";
+	std::cout << "as well as an upper bound to stop counting at.\n";
+	
+	std::cin >> a >> b >> n;
+
+	fibonacci(a, b, vfibonacci, n);
+	// only call cout if there's anything to print, otherwise exit
+	if (vfibonacci.size() > 0) {
+		for (int i =0; i < (vfibonacci.size() -1); ++i)
+			std::cout << vfibonacci[i] << ", ";
+
+		// print the last element, with slightly different formatting
+		std::cout << vfibonacci.back() << ".";
+		std::cout << std::endl;
 	}
 	return 0;
 }
-	
